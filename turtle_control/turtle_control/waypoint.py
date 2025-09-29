94ef20fed5be7ba4d13fee51cf812487333f6873
rishika2024 <berarishika@gmail.com>
2025-09-28 23:41:43 -0500

    debuging the load service

--- turtle_control/turtle_control/waypoint.py
+++ turtle_control/turtle_control/waypoint.py
@@ -1,9 +1,16 @@
-from geometry_msgs.msg import Twist, Vector3
+from geometry_msgs.msg import Twist
+
+from turtle_interfaces.srv import WayPoints
 
 import rclpy
 from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException
 from std_srvs.srv import Empty
+
+from turtlesim_msgs.srv import TeleportAbsolute
+
+import math
+
 
 
 class Waypoint(Node):
@@ -15,19 +22,26 @@ class Waypoint(Node):
         self.srv = self.create_service(Empty, 'toggle', self.toggle_callback)
         self.cmd_vel_pub = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
 
+        self.srv_load= self.create_service(WayPoints, 'load', self.load_callback)
+
         self.MOVING = True
         self.STOP = False
         self.state = self.STOP
+
+        self.client = self.create_client(Empty, '/reset') #to restart turtlesim
+
+        
+        self.teleport = self.create_client(TeleportAbsolute, 'turtle1/teleport_absolute')
         
     def toggle_callback(self, request, response):
 
         if self.state == self.STOP:
             self.state = self.MOVING
-            self.get_logger().info(f"MOVING!!!")
+            self.get_logger().info("MOVING!!!")
 
         else:
             self.state = self.STOP
-            self.get_logger().info(f"STOPPING!!!")
+            self.get_logger().info("STOPPING!!!")
 
         return response    
         
@@ -45,20 +59,76 @@ class Waypoint(Node):
         else:
             msg.linear.x = 0.0
             msg.angular.z = 0.0
-            self.cmd_vel_pub.publish(msg)         
+            self.cmd_vel_pub.publish(msg)      
+
+    def draw_x(self, x, y):
+
+        request = TeleportAbsolute.Request()
+
+        request.x = x+1
+        request.y = y+1        
+        self.teleport.call_async(request)
+
+        request.x = x-1
+        request.y = y-1        
+        self.teleport.call_async(request)
+
+        request.x = x
+        request.y = y        
+        self.teleport.call_async(request)
+
+        request.x = x-1
+        request.y = y+1        
+        self.teleport.call_async(request)
+
+        request.x = x+1
+        request.y = y-1        
+        self.teleport.call_async(request)
+
+        request.x = x
+        request.y = y        
+        self.cli.call_async(request)    
+    
 
 
+    def load_callback(self, request, response):
+        self.get_logger().info("load service")
 
+        rclpy.spin_until_future_complete(self, self.client.call_async(Empty.Request()))
 
+        waypoints = request.waypoints
+
+        for wp in waypoints:
+            self.draw_x(wp.x, wp.y) #drawing x at all the waypoints
+
+        request = TeleportAbsolute.Request() #teleporting the turtle back to the 1st waypoint
+        request.x = waypoints[0].x
+        request.y = waypoints[0].y        
+        self.teleport.call_async(request)
+
+        self.state = self.STOP
+
+        distance = 0.0
+
+        for i in range(len(waypoints)-1):
+            p1 = waypoints[i]
+            p2 =  waypoints[i+1]
+            distance+= math.dist([p1.x, p1.y], [p2.x, p2.y])
+
+        distance += math.dist([waypoints[0].x, waypoints[0].y], [waypoints[len(waypoints)-1].x,waypoints[len(waypoints)-1].y ])
+
+        response.distance = distance
+
+        self.get_logger().info(f"Total cycle distance = {distance:.2f}")
+        return response
+
+        
 def main(args=None):
-    try:
-        with rclpy.init(args=args):
-            node = Waypoint()
-
-            rclpy.spin(node)
-    except (KeyboardInterrupt, ExternalShutdownException):
-        pass
-
+    rclpy.init(args=args)            
+    node = Waypoint()                
+    rclpy.spin(node)                 
+    node.destroy_node()          
+    rclpy.shutdown() 
 
 if __name__ == '__main__':
     main()
